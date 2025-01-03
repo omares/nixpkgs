25c98cfbbea6feefdf2d79f77e390c9e322322b5
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-01-03 01:31:00 +0100

    python313Packages.pyflick: 0.0.2 -> 1.1.2
    https://github.com/ZephireNZ/PyFlick/compare/refs/tags/v0.0.2...v1.1.2

--- pkgs/development/python-modules/pyflick/default.nix
+++ pkgs/development/python-modules/pyflick/default.nix
@@ -1,27 +1,36 @@
 {
   lib,
   buildPythonPackage,
-  pythonOlder,
-  fetchPypi,
+  fetchFromGitHub,
+  setuptools,
+  setuptools-scm,
   aiohttp,
+  json-api-doc,
   python-dateutil,
 }:
 
 buildPythonPackage rec {
   pname = "pyflick";
-  version = "0.0.2";
-  format = "setuptools";
+  version = "1.1.2";
+  pyproject = true;
 
-  disabled = pythonOlder "3.6";
-
-  src = fetchPypi {
-    pname = "PyFlick";
-    inherit version;
-    sha256 = "705c82d8caedfff19117c8859cc1b4f56e15ab8dbc0d64d63b79d8634007897f";
+  src = fetchFromGitHub {
+    owner = "ZephireNZ";
+    repo = "PyFlick";
+    tag = "v${version}";
+    hash = "sha256-Csm5gXMIGEhHgzN/7sO/1iM/wZklI2Jc0C69tgYWxnQ=";
   };
 
-  propagatedBuildInputs = [
+  build-system = [
+    setuptools
+    setuptools-scm
+  ];
+
+  pythonRelaxDeps = [ "aiohttp" ];
+
+  dependencies = [
     aiohttp
+    json-api-doc
     python-dateutil
   ];
 
